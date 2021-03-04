396ce13ef07bb3c55ea95479fbf3cbc5edeaf275
Miguel Ramos <miguel.ramos@prozis.com>
2021-03-04 18:42:16 +0000

    feat: initial window connection example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { Courier } from './courier';
 
-const courier = new Courier({
-  api: 'http://localhost:8883/v1',
-  token: ''
-});
+window.addEventListener('load', () => {
+  const courier = new Courier({
+    api: 'http://localhost:8883/v1',
+    token: ''
+  });
 
-console.dir(courier);
+
+
+  courier.sign().then((signature) => {
+    const socket = courier.connect(signature);
+
+    socket.onmessage = (payload) => {
+      console.dir(payload);
+    };
+
+    courier.subscribe('system/events');
+  });
+});
